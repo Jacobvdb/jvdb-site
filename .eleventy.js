2d69edf20e8f324a36c2c6ec998d3e859bc9d1e0
jacobvdb <jacob@bkper.com>
2025-01-15 08:28:55 -0300

    Basic structue and some initial functionality

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("favicon.svg");
+    eleventyConfig.addPassthroughCopy("css");
+    eleventyConfig.addPassthroughCopy("js");
     
     return {
         dir: {
