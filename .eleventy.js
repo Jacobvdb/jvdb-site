87ef31c6c36e7aacdb19f3473ef469c548037ba3
jacobvdb <jacob@bkper.com>
2025-01-17 15:05:51 -0300

    several adjustments

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,24 @@
+const { DateTime } = require("luxon");
+
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("favicon.svg");
     eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("js");
+
+    eleventyConfig.addFilter("formatUTCDate", function(dateObj) {
+        return DateTime.fromJSDate(new Date(dateObj), { zone: "utc" }).toFormat("MM-dd-yyyy");
+      });
     
     return {
         dir: {
             input: ".",
-            output: "_site"
+            output: "_site",
+            layouts: "_layouts"
         },
         templateFormats: ["md", "njk", "html","liquid"],
        
     };
-};
+
+    
+     
+}
--- index.md
+++ index.md
@@ -1,16 +1,5 @@
 ---
 layout: "layout.html"
-title: "Jacob van den Berg"
+title: "Jacob van den Berg "
 ---
 
-
-
-{% for post in collections.post %}
-
-### <a href=" {{ post.url }} "><h2>{{ post.data.title }}</h2></a>
-
-## {{ post.data.subtitle }}
-
-{{ post.data.description }} 
-
-{%  endfor %} 
